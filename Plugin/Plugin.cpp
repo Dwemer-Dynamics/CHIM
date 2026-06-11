c969eb67cd4864c31bada5a4826e2c63bc5e469d
RANGROO <reecemeakings@gmail.com>
2026-06-10 19:36:01 -0700

    2.7.3 update

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -44,8 +44,8 @@
 
 using json = nlohmann::json;
 
-#define PLUGIN_VERSION "2.8.2"
-#define PLUGIN_RELEASE_DATE "2026-06-07"
+#define PLUGIN_VERSION "2.7.3"
+#define PLUGIN_RELEASE_DATE "2026-06-11"
 
 static void AddCachedSpeechAudience(json& speechPayload, const std::string& reason);
 
