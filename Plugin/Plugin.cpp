c01d3ac047511101c7019ef7b894526f128bf6f1
RANGROO <reecemeakings@gmail.com>
2026-06-10 19:43:44 -0700

    acutal 2.8.3 update

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -44,7 +44,7 @@
 
 using json = nlohmann::json;
 
-#define PLUGIN_VERSION "2.7.3"
+#define PLUGIN_VERSION "2.8.3"
 #define PLUGIN_RELEASE_DATE "2026-06-11"
 
 static void AddCachedSpeechAudience(json& speechPayload, const std::string& reason);
