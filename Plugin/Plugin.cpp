d1a912eb963844509bff57bbb72e822d142abbc3
RANGROO <reecemeakings@gmail.com>
2026-07-18 14:50:17 -0700

    Sync bundled server plugins on game load

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -47,6 +47,7 @@
 #include "SpatialAwareness.h"
 #include "SpatialSnapshotManager.h"
 #include "VRItemAwareness.h"
+#include "ServerPluginSync.h"
 
 using json = nlohmann::json;
 
@@ -7832,6 +7833,7 @@ OnLoadedGame {
         // Detect and upload CSV import data files on first load (only once per session)
         if (!importDataDetectionDone) {
             DetectAndUploadImportDataFiles();
+            ScheduleServerPluginSync();
             importDataDetectionDone = true;
         }
 
