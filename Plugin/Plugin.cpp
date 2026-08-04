161c267312b98b7375f4fb33b1eb7ee610cd5466
RANGROO <reecemeakings@gmail.com>
2026-08-03 17:28:29 -0700

    Bump CHIM version to 3.2.1

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -53,8 +53,8 @@
 
 using json = nlohmann::json;
 
-#define PLUGIN_VERSION "3.2.0"
-#define PLUGIN_RELEASE_DATE "2026-08-01"
+#define PLUGIN_VERSION "3.2.1"
+#define PLUGIN_RELEASE_DATE "2026-08-03"
 
 const char* GetPluginVersion()
 {
